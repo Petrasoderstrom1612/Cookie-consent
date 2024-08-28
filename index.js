dd9b81caded0b8e2abdbc55afb12a4259f5a5b3d
Petra Söderström <101672601+Petrasoderstrom1612@users.noreply.github.com>
2024-08-28 13:46:26 +0200

    added a new part to the consent form with innerText

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
 const modal = document.getElementById("modal")
 const modalCloseBtn = document.getElementById("modal-close-btn")
+const consentForm = document.getElementById("consent-form")
+const modalText = document.getElementById("modal-text")
 
 setTimeout(function(){
     modal.style.display = "flex"
 },1500)
 
 modalCloseBtn.addEventListener("click", function(){modal.style.display = "none"})
+
+consentForm.addEventListener("submit",function(e){
+    e.preventDefault()
+    console.log("submitted")
+    modalText.innerHTML = `
+    <div class="modal-inner-loading">
+    <img src="images/loading.svg" class="loading">
+    <p id="upload-text">Uploading your data to the dark web...</p>
+    </div>`
+    addEventListener(function(){
+        document.getElementById("upload-text").innerText = `Making the sale...` //since compiler reads html first and then JS, you cannot declare the dom on top of the page, the dom will have passed prior to being used in html and hence not used
+    },1500)
+})
+
+

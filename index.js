ce8d757443cb635e9bc9b3672ef00437edd99df3
Petra Söderström <101672601+Petrasoderstrom1612@users.noreply.github.com>
2024-08-28 14:11:24 +0200

    added a gif and cover the entire modalInner

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const modal = document.getElementById("modal")
 const modalCloseBtn = document.getElementById("modal-close-btn")
 const consentForm = document.getElementById("consent-form")
 const modalText = document.getElementById("modal-text")
+const modalInner = document.getElementById("modal-inner")
 
 setTimeout(function(){
     modal.style.display = "flex"
@@ -14,12 +15,21 @@ consentForm.addEventListener("submit",function(e){
     console.log("submitted")
     modalText.innerHTML = `
     <div class="modal-inner-loading">
-    <img src="images/loading.svg" class="loading">
+    <img src="loading.svg" class="loading">
     <p id="upload-text">Uploading your data to the dark web...</p>
     </div>`
-    addEventListener(function(){
+
+    setTimeout(function(){
         document.getElementById("upload-text").innerText = `Making the sale...` //since compiler reads html first and then JS, you cannot declare the dom on top of the page, the dom will have passed prior to being used in html and hence not used
     },1500)
+
+    setTimeout(function(){
+        modalInner.innerHTML = `<h2>Thanks you sucker! </h2>
+    <p>We just sold the rights to your eternal soul.</p>
+    <div class="idiot-gif">
+        <img src="laughing.gif">
+    </div>` 
+    },3000)
 })
 
 
